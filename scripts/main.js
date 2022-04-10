04963b8695c1c101cd7aa1b573b2b62d4be97d99
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-04-10 14:29:04 -0400

    got 3 day forecast populating

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,20 @@
+// ULTIMATE WEATHER APP
+
+
+//API Link
 const API = "https://wttr.in/"
 
 // form submit event listener
 const locationForm = document.querySelector("#locationForm");
 locationForm.addEventListener("submit", (e) => handleSubmit(e));
 
+//global variables for key html elements
 const mainWeather = document.getElementById('mainWeather');
+const previousSearches = document.getElementById('previousSearches');
+const threeDay = document.getElementById('threeDay');
+const today = document.getElementById('today');
+const tomorrow = document.getElementById('tomorrow');
+const dayAfter = document.getElementById('dayAfter');
 
 
 
@@ -16,7 +26,10 @@ const handleSubmit = (e) => {
     if (!input) return;
     target.location.value = '';
 
+    fetcher(input);
+}
 
+const fetcher = (input) => {
     fetch(`${API}${input.split(' ').join('+')}?format=j1`)
         .then(res => res.json())
         .then(res => populate(res, input))
@@ -25,9 +38,15 @@ const handleSubmit = (e) => {
 
 
 const populate = (res, input) => {
-    console.log (res);
+    
+    //clears out current information (if any)
     mainWeather.textContent = '';
+    today.textContent = '';
+    tomorrow.textContent = '';
+    dayAfter.textContent = '';
 
+
+    //populate main area
     const location = document.createElement("p");
     location.setAttribute("id", "locationP");
     location.setAttribute("class", "mainWeatherP");
@@ -54,7 +73,40 @@ const populate = (res, input) => {
     currently.innerHTML = `<strong>Currently: </strong>Feels Like ${res.current_condition[0].FeelsLikeF}${String.fromCharCode(176)}F`;
 
     mainWeather.append(location, area, region, country, currently)
+    
+    //populate 3 day forecast
+    const average1 = document.createElement("p");
+    const max1 = document.createElement("p");
+    const min1 = document.createElement("p");
 
+    average1.innerHTML = `<strong>Average Temperature:</strong>${res.weather[0].avgtempF}${String.fromCharCode(176)}F`;
+    max1.innerHTML = `<strong>Max Temperature:</strong>${res.weather[0].maxtempF}${String.fromCharCode(176)}F`;
+    min1.innerHTML = `<strong>Min Temperature:</strong>${res.weather[0].mintempF}${String.fromCharCode(176)}F`;
+    
+    today.innerHTML = `<h3 class="threeDayHeaders">Today</h3>`;
+    today.append(average1, max1, min1);
+
+    const average2 = document.createElement("p");
+    const max2 = document.createElement("p");
+    const min2 = document.createElement("p");
+
+    average2.innerHTML = `<strong>Average Temperature:</strong>${res.weather[1].avgtempF}${String.fromCharCode(176)}F`;
+    max2.innerHTML = `<strong>Max Temperature:</strong>${res.weather[1].maxtempF}${String.fromCharCode(176)}F`;
+    min2.innerHTML = `<strong>Min Temperature:</strong>${res.weather[1].mintempF}${String.fromCharCode(176)}F`;
+
+    tomorrow.innerHTML = `<h3 class="threeDayHeaders">Tomorrow</h3>`;
+    tomorrow.append(average2, max2, min2);
+
+    const average3 = document.createElement("p");
+    const max3 = document.createElement("p");
+    const min3 = document.createElement("p");
+
+    average3.innerHTML = `<strong>Average Temperature:</strong>${res.weather[2].avgtempF}${String.fromCharCode(176)}F`;
+    max3.innerHTML = `<strong>Max Temperature:</strong>${res.weather[2].maxtempF}${String.fromCharCode(176)}F`;
+    min3.innerHTML = `<strong>Min Temperature:</strong>${res.weather[2].mintempF}${String.fromCharCode(176)}F`;
+
+    dayAfter.innerHTML = `<h3 class="threeDayHeaders">Day After Tomorrow</h3>`;
+    dayAfter.append(average3, max3, min3);
 }
 
 const handleError = (err) => {
