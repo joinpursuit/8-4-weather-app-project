1bf547afcc78d2a7b64a365f9cbc9bb2010eaca7
Tom LaTulipe <tlatulipe@gmail.com>
2022-04-12 20:55:59 -0400

    Making the API call - getting good data back

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const API_URL = "https://wttr.in/";
 const wttrJSON = "?format=j1";
+const currentWeather  = document.getElementById("currentWeather");
 
 const searchForm = document.getElementById("form");
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(apiCall(searchForm.location.value));
-    //console.log(searchForm.location.value);
+    apiCall(searchForm.location.value);
+    searchForm.location.value="";
 });
 
 const apiCall = ((location) => {
-    const search = API_URL + location + wttrJSON;
+    let search = API_URL + location + wttrJSON;
     let results;
     fetch(search)
     .then((weather) => weather.json())
     .then((weather) => {
-        //console.log(weather);
-        results = weather;
+        pagePopulation(weather, location);
     })
     .catch((error) => console.log("ERROR HERE: ", error));
-    return results;
+});
+
+const pagePopulation = ((weather, location) => {
+    console.log(weather);
+    let nearestArea = weather.nearest_area[0].areaName[0].value;
+    let region = weather.nearest_area[0].region[0].value;
+    let country = weather.nearest_area[0].country[0].value;
+    let currentTemp = weather.current_condition[0].FeelsLikeF;
+    let rainChance = weather.weather[0].hourly[0].chanceofrain;
 });
