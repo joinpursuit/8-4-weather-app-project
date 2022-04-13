6f16f6771d6dc60653528f8b0a36ed1631f9e62d
Zane <zaneahmed4@gmail.com>
2022-04-12 21:11:11 -0400

    second commit

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ let data
 const form = document.querySelector("form")
 const main = document.querySelector("main")
 const current = document.querySelector("#current")
+const today = document.querySelector("#today")
+const dayAfter = document.querySelector("#dayAfter")
+const sidebar = document.querySelector(".sidebar")
+const ul = document.querySelector(".sidebar ul")
+
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
@@ -13,6 +18,11 @@ form.addEventListener("submit", (event) => {
     .then((json) => {
         data = json
         weather(data)
+        console.log(data)
+        const li = document.createElement("li")
+        li.textContent = `${inputText.charAt(0).toUpperCase() + inputText.slice(1)}`
+        ul.append(li)
+        form.reset()
     })
     .catch((error) => {
         alert(error)
@@ -20,10 +30,6 @@ form.addEventListener("submit", (event) => {
 })
 
 function weather (data){
-//city name = array data.nearest_area[0].areaName[0].value
-// data.current_condition[0].FeelsLikeF in aside
-//add searches to sidebar ul 
-
 //current weather article on top
     const currenth1 = document.createElement('h1')
     currenth1.textContent = `${data['nearest_area'][0].areaName[0].value}`
@@ -40,4 +46,48 @@ function weather (data){
     const currentp4 = document.createElement('p')
     currentp4.textContent = `Currently: Feels Like ${data['current_condition'][0]['FeelsLikeF']}°F`
     current.append(currentp4)
+
+    //today- average, max, min temp
+    const todayh2 = document.createElement('h2')
+    todayh2.textContent = `Today`
+    today.append(todayh2)
+    const todayp1 = document.createElement('p')
+    todayp1.textContent = `Average Temperature: ${data.weather[0].avgtempF}°F`
+    today.append(todayp1)
+    const todayp2 = document.createElement('p')
+    todayp2.textContent = `Max Temperature: ${data.weather[0].maxtempF}°F`
+    today.append(todayp2)
+    const todayp3 = document.createElement('p')
+    todayp3.textContent = `Min Temperature: ${data.weather[0].mintempF}°F`
+    today.append(todayp3)
+
+    //tomorrow- average, max, min temp
+    const tomorrowh2 = document.createElement('h2')
+    tomorrowh2.textContent = `Tomorrow`
+    tomorrow.append(tomorrowh2)
+    const tomorrowp1 = document.createElement('p')
+    tomorrowp1.textContent = `Average Temperature: ${data.weather[1].avgtempF}°F`
+    tomorrow.append(tomorrowp1)
+    const tomorrowp2 = document.createElement('p')
+    tomorrowp2.textContent = `Max Temperature: ${data.weather[1].maxtempF}°F`
+    tomorrow.append(tomorrowp2)
+    const tomorrowp3 = document.createElement('p')
+    tomorrowp3.textContent = `Min Temperature: ${data.weather[1].mintempF}°F`
+    tomorrow.append(tomorrowp3)
+
+    //day after- average, max, min temp
+    const dayAfterh2 = document.createElement('h2')
+    dayAfterh2.textContent = `Day After Tomorrow`
+    dayAfter.append(dayAfterh2)
+    const dayAfterp1 = document.createElement('p')
+    dayAfterp1.textContent = `Average Temperature: ${data.weather[2].avgtempF}°F`
+    dayAfter.append(dayAfterp1)
+    const dayAfterp2 = document.createElement('p')
+    dayAfterp2.textContent = `Max Temperature: ${data.weather[2].maxtempF}°F`
+    dayAfter.append(dayAfterp2)
+    const dayAfterp3 = document.createElement('p')
+    dayAfterp3.textContent = `Min Temperature: ${data.weather[2].mintempF}°F`
+    dayAfter.append(dayAfterp3)
+    //sidebar for previous searches- link and temp
+   
 }
