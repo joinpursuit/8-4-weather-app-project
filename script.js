dfc9554e52c857cc576fd55c76987e1509ba5d62
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-04-18 17:58:38 -0400

    "Psuedo"

--- script.js
+++ script.js
@@ -1,214 +1,278 @@
+// give the url a variable name 
+const BASE_URL = "https://wttr.in";
+// create a variable for the form event
+const searchForm = document.querySelector("form.event");
+// create a GLOBAL variable for a location search by the user
+let locationSearch = "";
+// create a variable for the convertForm accessed by ID
+const locationInfo = document.getElementById("locationInfo");
+// create a variable for the weather for today accessed by ID
+const convertForm = document.getElementById("convertForm");
+// create a variable for the temperature conversion results accessed by ID
+const tempCalcResult = document.getElementById("tempCalcResult");
+// create a variable for the location info submitted by the user accessed by ID
+const weatherToday = document.getElementById("weatherToday");
+// create a variable for the weather for tomorrow accessed by ID
+const weatherTomorrow = document.getElementById("weatherTomorrow");
+// creat a variable for the weather for the day after accessed by ID
+const weatherDayAfter = document.getElementById("weatherDayAfter");
+// create a GLOBAL variable for the user location search
+const displayPlaceHolder = document.getElementsByClassName(
+  "display-placeholder"
+)[0];
+// create a variable for the previous search listings accessed by ID
+const previousSearchUl = document.getElementById("previousSearchUl");
+// create a varaible for the previous search accessed by ID
+const previousSearchP = document.getElementById("previousSearchP");
 
-const form = document.querySelector('form');
-const cloc = document.getElementById("citylocation")
-const carea = document.getElementById("cityarea")
-const cregion = document.getElementById("cityregion")
-const ccountry= document.getElementById("citycountry")
-const ccurrent= document.getElementById("citycurrent")
-const ps=document.querySelector(".previous-search")
-const ul=document.querySelector(".previous-search-list")
-const s=document.getElementsByTagName("span")
-// previousSearches = [];
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
 
-    // let input = 
-    let location = event.target.location.value;
-    getWeatherInfo(location)
-    ps.remove()
-    
-    form.reset();
+// add an event listener to the form when submit is used
+searchForm.addEventListener("submit", (e) => {
+  // prevent the page from refreshing 
+  e.preventDefault();
 
+  // create a variable for the box the user inputs the location with the ID location 
+  const input = document.querySelector("#location");
+  // remove the white spaces around the user input value inside the blank location search
+  locationSearch = input.value.trim();
 
+  // if the location search is blank 
+  if (!locationSearch) {
+    // give the user an alert message
+    alert("Location cannot be empty");
+  } else {
+    // else run the function get weather search with the search location
+    getWeatherSearch(locationSearch, createPrevSearch);
+  }
+  // with the user input value
+  input.value = "";
 });
 
-function getWeatherInfo(location){
-    let url = `https://wttr.in/${location}?format=j1`;
 
-    console.log(url)
+// add an event listner to the convert Fahrenheit to Celcius form with submit is used - WIDGET
+convertForm.addEventListener("submit", (e) => {
+  // prevent the page from refreshing
+  e.preventDefault();
 
-    fetch(url)
+  // create a variable for the temp to conversion input box
+  const tempInput = document.getElementById("temp-to-convert");
+  // create a variable for the value inside the temp conversion input box
+  let value = tempInput.value;
+
+  // if the temperature conversion input box is empty
+  if (!value) {
+    // give the user an alert message
+    alert("Temperature value cannot be empty");
+    return;
+  }
+
+  // create radio buttons for celcius conversion accessed by ID 
+  let cRadio = document.getElementById("to-c");
+    // create radio buttons for celcius conversion accessed by ID 
+  let fRadio = document.getElementById("to-f");
+
+  // if celcuis radio button is checked 
+  if (cRadio.checked) {
+    // celcius conversion equal the value(in Farenheit) - 32 * (5/9)
+    let cel = (value - 32) * (5 / 9);
+    // celcius conversion to the 2 decimal place 
+    cel = cel.toFixed(2);
+    // conversion result number made a string (text.Content doesn't do that)
+    tempCalcResult.innerText = cel;
+
+    // else is farenheit button is checked 
+  } else if (fRadio.checked) {
+    // farenheit conversion equal to value(in Celcius) * 9 / 5 + 32
+    let fah = (value * 9) / 5 + 32;
+    // farhenheit conversion to the 2 decimal place
+    fah = fah.toFixed(2);
+    // conversion result number made a string (text.Content doesn't do that)
+    tempCalcResult.innerText = fah;
+  } else {
+    // else give an alert to provide number to convert
+    alert("Please select a unit");
+  }
+});
+
+// Make a request to wttr API with the user entered location
+// create a function used in out event listeners with the users location search 
+function getWeatherSearch(locationSearch, createPrevSearch) {
+  let url = `${BASE_URL}/${locationSearch}?format=j1`;
+  // fetch API INFORMTION through a promise from JS to JSON
+  fetch(url)
     .then((res) => res.json())
-    .then((data) => {
-        console.log("DATA:",data)
-
-        // ASSIGN VARIABLES TO THE DATA RECEIVED FROM API VIA FETCH .THEN .. WHICH ARE ARRANGED AS AN ARRAY OF OBJECTS
-
-        // create a variable for the city location which in API form is access through nearest_area array first element areaName array first element key value - MELBORNE
-        let cityLocation  = data.nearest_area[0].areaName[0].value
-        // create a variable for the country name which in API form is access through nearest_area array first element country array first element key value - AUSTRALIA
-        let countryName = data.nearest_area[0].country[0].value
-        // create a variable for the region which in API form is access through nearest_area array first element region array first element key value - ONTARIO
-        let region = data.nearest_area[0].region[0].value
-        // create a variable for the current condition which in API form is access through current_consition array first element areaName array second element key value - FeelsLikeF 
-        let currentCondition = data.current_condition[0].FeelsLikeF
-
-        console.log(cityLocation)
-
-        
-        cloc.innerHTML=`${location}`
-        carea.innerHTML =`<b>Nearest Area</b> : ${cityLocation}`
-        cregion.innerHTML =`<b>Region</b> : ${region}`
-        ccountry.innerHTML =`<b>Country </b>: ${countryName}`
-        ccurrent.innerHTML=`<b>Currently : </b> Feels like ${currentCondition} F`
-
-
-        // CREATE 3 DIVS WITH TODAY TOMORROW AND THE DAYAFTER TOMORROW TEMPERATURES
-        //today
-
-        // create a variable for today's average temp which in API for is access through the weather array first element avgtempF key 
-        const todayAvgTemp=data.weather[0].avgtempF
-        // create a variable for today's max temp which in API for is access through the weather array first element maxtempF key 
-        const todayMaxTemp = data.weather[0].maxtempF
-        // create a variable for today's min temp which in API for is access through the weather array first element mintempF key 
-        const todayMinTemp = data.weather[0].mintempF
-
-        //tomorrow
-        const tomorrowAvgTemp = data.weather[1].avgtempF
-        const tomorrowMaxTemp =data.weather[1].maxtempF
-        const tomorrowMinTemp =data.weather[1].mintempF
-
-        //dayAfterTomorrow
-        const dayAfterAvgTemp = data.weather[2].avgtempF
-        const dayAfterMaxTemp =data.weather[2].maxtempF
-        const dayAfterMinTemp =data.weather[2].mintempF
-
-
-        // console.log(todayAvgTemp)
-        // console.log(tomorrowAvgTemp)
-        // console.log(dayAfterAvgTemp)
-
-        const today=document.getElementById("today")
-
-        th1 =document.createElement("strong")
-        th1.textContent="Today"
-        p1=document.createElement("p")
-        p1.innerHTML=`<strong>Average Temperature :</strong> ${todayAvgTemp}F`
-        p2=document.createElement("p")
-        p2.innerHTML=`<strong>Max Temperature : </strong> ${todayMaxTemp} F`
-        p3=document.createElement("p")
-        p3.innerHTML=`<strong>Min Temperature : </strong>${todayMinTemp} F`
-
-        today.appendChild(th1)
-        today.appendChild(p1)
-        today.appendChild(p2)
-        today.appendChild(p3)
-
-        const tomorrow=document.getElementById("tomorrow")
-
-        th1 =document.createElement("strong")
-        th1.textContent="Tomorrow"
-        p4=document.createElement("p")
-        p4.innerHTML=`<strong>Average Temperature :</strong> ${dayAfterAvgTemp} F`
-        p5=document.createElement("p")
-        p5.innerHTML=`<strong>Max Temperature : </strong> ${dayAfterMaxTemp} F`
-        p6=document.createElement("p")
-        p6.innerHTML=`<strong>Min Temperature : </strong>${dayAfterMinTemp} F`
-
-        tomorrow.append(th1)
-        tomorrow.append(p4)
-        tomorrow.append(p5)
-        tomorrow.append(p6)
-
-        const dayAfter=document.getElementById("dayAfter")
-
-        th1 =document.createElement("strong")
-        th1.textContent="dayAfter"
-        p7=document.createElement("p")
-        p7.innerHTML=`<strong>Average Temperature :</strong> ${tomorrowAvgTemp} F`
-        p8=document.createElement("p")
-        p8.innerHTML=`<strong>Max Temperature : </strong> ${tomorrowMaxTemp} F`
-        p9=document.createElement("p")
-        p9.innerHTML=`<strong>Min Temperature : </strong> ${tomorrowMinTemp} F`
-
-        dayAfter.append(th1)
-        dayAfter.append(p7)
-        dayAfter.append(p8)
-        dayAfter.append(p9)
-        
-        const p = document.createElement('p')
-        // previous_city = {"cityName":cityLocation,"currentCondition":currentCondition}
-        // previousSearches.push(previous_city)
-        // console.log(previousSearches)
-
-        previousSearch(data,location);
-        
-
-
-
-    })
-    .catch((err => alert(err)))
-
-}
-const convertForm = document.getElementById("convertForm")
-convertForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const tempInput = document.getElementById("temp-to-convert");
-    let value = tempInput.value;
-
-    if(!value){
-        alert("Temperature value cannot be empty")
-        return;
-    }
-    let cRadio = document.getElementById("to-c");
-    let fRadio = document.getElementById("to-f");
-
-    if(cRadio.checked){
-        let cel = (value - 32) * (5 / 9);
-        cel = cel.toFixed(2);
-        tempCalcResult.textContent = cel;
-    } else if (fRadio.checked){
-        let fah = (value * 9) / 5 + 32;
-        fah = fah.toFixed(2);
-        tempCalcResult.textContent = fah;
-    } else {
-        alert("Please select a unit");
-    }
-});
-
-
-function  previousSearch(data,location) {
-    const li=document.createElement("li");
-    const a= document.createElement("a");
-
-    a.textContent=`${data.nearest_area[0].areaName[0].value}`
-    a.setAttribute("href","#")
-    li.textContent=`${data.current_condition[0].FeelsLikeF} F`;
-    ul.append(li);
-    li.prepend(a);
-
-    a.addEventListener('click',(event)=>{
-        
-
-        getWeatherInfo(location)
-    })
-    
+    // assign that API data to a function with the data as a parameter and previous search info
+    .then((data) => parseNeededData(data, createPrevSearch))
+    .catch((error) => console.log("Error: ", error));
 }
 
+/*
+Needed Data to create main section from searchForm event : 
+    Area, Region, Country, FeelsLike
+    (Today, Tomorrow, DayAfterTomorrow):
+        Avg Temp,
+        Max Temp,
+        Min Temp
+    Chance of Sunshine,
+    Chance of Rain,
+    Chance of Snow
+*/
 
-    
-// function iconData(data, location){
+// create a function to pull the specific data from fetch to create our main section  and the info from the previous search
+function parseNeededData(jsonData, createPrevSearch) {
+  // create variables for key value of average temp -- ICONS
+  let avgTempKey = "avgtempF";
+  let maxTempKey = "maxtempF";
+  let minTempKey = "mintempF";
+  // create variables for the array index values --- MAIN DISPLAY
+  let feelsLike = jsonData.current_condition[0].FeelsLikeF;
+  let area = jsonData.nearest_area[0].areaName[0].value;
+  let region = jsonData.nearest_area[0].region[0].value;
+  let country = jsonData.nearest_area[0].country[0].value;
 
-//  const article = document.getElementById("icon")
+  // create variables for the weather for today.tomorrow and day after --- 3 DAYS 
+  let today = {
+    avgTemp: jsonData.weather[0][avgTempKey],
+    maxTemp: jsonData.weather[0][maxTempKey],
+    minTemp: jsonData.weather[0][minTempKey],
+  };
+  let tomorrow = {
+    avgTemp: jsonData.weather[1][avgTempKey],
+    maxTemp: jsonData.weather[1][maxTempKey],
+    minTemp: jsonData.weather[1][minTempKey],
+  };
+  let dayAfter = {
+    avgTemp: jsonData.weather[2][avgTempKey],
+    maxTemp: jsonData.weather[2][maxTempKey],
+    minTemp: jsonData.weather[2][minTempKey],
+  };
+  let chanceOfRain = jsonData.weather[0].hourly[0].chanceofrain;
+  let chanceOfSnow = jsonData.weather[0].hourly[0].chanceofsnow;
+  let chanceOfSunshine = jsonData.weather[0].hourly[0].chanceofsunshine;
 
-//  const img = document.createElement(img)
+  // call the functionall with the parameters we created that will update with the users specific location search --------------- ACTION TO UPDATE THOSE PARAMTERS WITH USER SEARCH 
+  updateHTML({
+    feelsLike,
+    area,
+    region,
+    country,
+    today,
+    tomorrow,
+    dayAfter,
+    chanceOfRain,
+    chanceOfSnow,
+    chanceOfSunshine,
+    createPrevSearch,
+  });
+}
 
-//  if(data.wearher[0].hourly.chanceofsunchsine >= 50){
-//      img.src = "./assets/icons8-summer.gif";
-//      img.alt = "sun";
-//     //  article.append(img);
-//  } else if (data.weather[0].hourly.chanceofrain >=50){
-//      img.src = "./assets/icons8-torrential-rain.gif"
-//      img.alt = "rain"
-//     //  article.append(img)
-//  } else if (data.weather[0].hourly.chanceofsnow >=50){
-//     img.src = "./assets/icons8-light-snow.gif"
-//     img.alt = "snow"
-//     // article.append(img)
-//  }
+// create a function that with the updated parameters from the users specific search location to attach a matching icon depending on the conditionlal statement ----- ({}) are used for multiple parameters ---- SPECIFY WHICH VALUES TO UPDATE WITH USER SEARCH 
+function updateHTML({
+  feelsLike,
+  area,
+  region,
+  country,
+  today,
+  tomorrow,
+  dayAfter,
+  chanceOfRain,
+  chanceOfSnow,
+  chanceOfSunshine,
+  createPrevSearch,
+}) {
+
+  // the middile section display will be blank until a user does a search
+  displayPlaceHolder.style.display = "none";
+  // the location search display will be centered in block form
+  locationInfo.style.display = "block";
+  // create a variable for imperfect user location search
+  let isImperfect = area.toLowerCase() !== locationSearch.toLowerCase();
+  // create a variable to display all the user location search information as follows;
+  // call the function to add icon with the parameters from max values
+  // header should print the user location search
+  // call function if location search is imperfect return the nearest area if not return the area
+  // call function to create region with user search  
+  // call function to create country with user seach etc...
+  let locationInfoHTML = `
+        ${addWeatherIcon(chanceOfRain, chanceOfSnow, chanceOfSunshine)}
+        <h2>${locationSearch}</h2>
+        ${createItem(isImperfect ? "Nearest Area" : "Area", area)}
+        ${createItem("Region", region)}
+        ${createItem("Country", country)}
+        ${createItem("Currently", `Feels Like ${feelsLike}&deg;F`)}
+        ${createItem("Chance of Sunshine", chanceOfSunshine)}
+        ${createItem("Chance of Rain", chanceOfRain)}
+        ${createItem("Chance of Snow", chanceOfSnow)}
+      `;
+
+      // updating the location info section after the functions have been called with users search parameters
+  locationInfo.innerHTML = locationInfoHTML;
+
+  // weather-info aside is set to display: none
+  // because it shows the border when it is empty
+  // so make it display none by default and
+  // make it visible when we need to show the data
+  document.querySelector(".weather-info").style.display = "grid";
+
+  // create a variable for the 3 boxes under the main weather information with today tomorrow and the dayAfter
+  let weatherTodayHTML = `
+    <h3>Today</h3>
+    ${createItem("Average Temperature", `${today.avgTemp}&deg;F`)}
+    ${createItem("Max Temperature", `${today.maxTemp}&deg;F`)}
+    ${createItem("Min Temperature", `${today.minTemp}&deg;F`)}
+  `;
+  let weatherTomorrowHTML = `
+    <h3>Tomorrow</h3>
+    ${createItem("Average Temperature", `${tomorrow.avgTemp}&deg;F`)}
+    ${createItem("Max Temperature", `${tomorrow.maxTemp}&deg;F`)}
+    ${createItem("Min Temperature", `${tomorrow.minTemp}&deg;F`)}
+  `;
+  let weatherDayAfterHTML = `
+    <h3>Day After Tomorrow</h3>
+    ${createItem("Average Temperature", `${dayAfter.avgTemp}&deg;F`)}
+    ${createItem("Max Temperature", `${dayAfter.maxTemp}&deg;F`)}
+    ${createItem("Min Temperature", `${dayAfter.minTemp}&deg;F`)}
 
 
+  `;
+  weatherToday.innerHTML = weatherTodayHTML;
+  weatherTomorrow.innerHTML = weatherTomorrowHTML;
+  weatherDayAfter.innerHTML = weatherDayAfterHTML;
 
 
+  // the previous search display with be empty until a user search is done
+  previousSearchP.style.display = "none";
+  // create a conditional event listenter to the users location search that will create a listing of the searches with link of the area and feels like temperature 
+  if (createPrevSearch) {
+    let prevSearchItem = document.createElement("li");
+    // add event listener to the seeach link when clicked will store the search area and calls the getWeather function with the search area again
+    prevSearchItem.addEventListener("click", () => {
+      locationSearch = area;
+      getWeatherSearch(area, false);
+    });
 
-// }
+    // update the previous search section a link with the search area and feels like in degree
+    prevSearchItem.innerHTML = `<a class="link">${area}</a> - ${feelsLike}&deg;F`;
+    // add theat item as a li to the ul
+    previousSearchUl.appendChild(prevSearchItem);
+  }
+}
+// function to create icons with the key value(average temp)and give the value in p tag
+function createItem(key, value) {
+  return `<p class="item"><span class="head">${key}</span> : ${value}</p>`;
+}
+
+// create a function to add the icon depending on the paramter values 
+function addWeatherIcon(rain, snow, sunshine) {
+  // create a variable equal to the max value of rain, snow or sunshine 
+  let maxValue = Math.max(rain, snow, sunshine);
+  // conditional statement to assign an icon on the max value 
+  if (maxValue == sunshine)
+    return '<img src="./assets/icons8-summer.gif" alt="sun" />';
+  if (maxValue == rain)
+    return '<img src="./assets/icons8-torrential-rain.gif" alt="rain" />';
+  if (maxValue == snow)
+    return '<img src="./assets/icons8-light-snow.gif" alt="snow" />';
+}
+
+
