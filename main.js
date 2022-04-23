9422721cbbcace90dd6c4bf4b83dd95b65560f06
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2022-04-23 00:30:18 -0400

    Continue work in progress.

--- main.js
+++ main.js
@@ -7,7 +7,14 @@ main.prepend(p1);
 // console.log(main);
 p1.textContent = 'Choose a location to view the weather.'
 // console.log(main);
+const noPreSe = document.createElement('p');
+noPreSe.textContent = 'No previous search';
+// console.log(noPreSe);
+const asideSection = document.querySelector('aside section');
+asideSection.append(noPreSe);
+const widget = document.querySelector('.widget');
 
+const form2 = document.querySelector('.widget form')
 
 
 
@@ -15,7 +22,7 @@ form.addEventListener('submit', (event)=>{
     event.preventDefault();
     // console.log(event.target);
     const city= document.querySelector('#city').value;
-    const location = document.getElementById('main #location');
+    // const location = document.getElementById('main #location');
     // console.log(city);
     fetch(`https://wttr.in/${city}?format=j1`)
     .then((response) => response.json())
@@ -30,12 +37,49 @@ form.addEventListener('submit', (event)=>{
         // console.log(country);
         const currentCondition = jsonWeatherData.current_condition[0].FeelsLikeF;
         // console.log(currentCondition);
+        const chanceSun = jsonWeatherData.weather[0].hourly[1].chanceofsunshine;
+        const chanceRain = jsonWeatherData.weather[0].hourly[1].chanceofrain;
+        const chanceSnow = jsonWeatherData.weather[0].hourly[1].chanceofsnow;
+
+
+        form.reset();
+
+        const mainArticle = document.querySelector('main article');
+
+
+        const sunImg = document.createElement('img');
+        const rainImg = document.createElement('img');
+        const snowImg = document.createElement('img');
+
+
+
+        snowImg.setAttribute('src','assets/icons8-light-snow.gif');
+        snowImg.setAttribute('alt', 'snow');
+        console.log(snowImg);
+        sunImg.setAttribute('src','assets/icons8-summer.gif');
+        sunImg.setAttribute('alt', 'sun');
+        rainImg.setAttribute('src', 'assets/icons8-torrential-rain.gif');
+
+        mainArticle.prepend(snowImg)
+
+
+        if (Number(chanceSnow) > 50) {
+            main.prepend(snowImg);
+            
+        }
+        else if (Number(chanceRain) > 50) {
+            main.prepend(rainImg);
+        }
+
+        else if (Number(chanceSun) > 50){
+            main.prepend(sunImg);
+        }
         const mainH2 = document.querySelector('main h2');
-        // console.log(mainArticle);
+       ;
         mainH2.textContent = city;
         // console.log(city);
-        const mainArticle = document.querySelector('main article');
-        // 
+        
+     
         mainArticle.innerHTML = `<strong>Area:</strong> ${area}<br><strong>Region:</strong> ${region}<br><strong>Country:</strong> ${country}<br><strong>Currently:</strong> Feels Like ${currentCondition}℉`;
 
         form.reset();
@@ -84,9 +128,49 @@ form.addEventListener('submit', (event)=>{
         p1.remove();
 
         
+        li.append(a);
+        ul.append(li);
+        li.append(a);
+        a.textContent = city;
+        li.innerHTML += `- ${currentCondition}℉`;
     })
-    
 
+
+    
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const ul = document.querySelector('aside section ul')
+
+
+
+    if(ul.childNodes.length >= 1) {
+        noPreSe.remove();``
+    }
+
+widget.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const temp = event.target.convert.value;
+    console.log(temp);
+    let convertTemp = document.querySelector('.#convert-to-temp').value;
+    convertTemp = Number(convertTemp);
+    const tempConvert = document.querySelector('.widget h4');
+
+    let celsius = (convertTemp - 32) * 5 / 9;
+    celsius = celsius.toFixed(2);
+
+    let fahrenheit = (convertTemp * 1.8) + 32;
+    fahrenheit = fahrenheit.toFixed(2);
+
+    if ( temp === 'f') {
+        tempConvert.innerHTML = fahrenheit; 
+    }
+    if (temp === 'c') {
+        tempConvert.innerHTML = celsius;
+    }
+
+    widget.reset();
+})
 
 })
 
